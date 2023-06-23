5fa57086c7c73a54f1d680f704b37f0c2f4bd378
ceack1990 <128190540+ceack1990@users.noreply.github.com>
2023-06-23 20:27:17 -0300

    Add files via upload

--- gestion_multas.sql
+++ gestion_multas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-06-2023 a las 00:51:18
+-- Tiempo de generación: 24-06-2023 a las 01:26:28
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -27,6 +27,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `multas` (
+  `Multa_ID` int(99) NOT NULL,
+  `Patente_ID` int(99) NOT NULL,
   `Razon` varchar(99) NOT NULL,
   `Monto` int(11) NOT NULL,
   `Estado` varchar(99) NOT NULL,
@@ -40,7 +42,7 @@ CREATE TABLE `multas` (
 --
 
 CREATE TABLE `patentes` (
-  `ID` int(99) NOT NULL,
+  `Patente_ID` int(99) NOT NULL,
   `nombre` varchar(99) NOT NULL,
   `dni` varchar(9) NOT NULL,
   `dirección` varchar(99) NOT NULL,
@@ -48,6 +50,42 @@ CREATE TABLE `patentes` (
   `marca` varchar(99) NOT NULL,
   `año` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `multas`
+--
+ALTER TABLE `multas`
+  ADD PRIMARY KEY (`Multa_ID`),
+  ADD KEY `Patente_ID` (`Patente_ID`);
+
+--
+-- Indices de la tabla `patentes`
+--
+ALTER TABLE `patentes`
+  ADD PRIMARY KEY (`Patente_ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `multas`
+--
+ALTER TABLE `multas`
+  MODIFY `Multa_ID` int(99) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `multas`
+--
+ALTER TABLE `multas`
+  ADD CONSTRAINT `multas_ibfk_1` FOREIGN KEY (`Patente_ID`) REFERENCES `patentes` (`Patente_ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
